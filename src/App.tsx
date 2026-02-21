9a2004e9fba91c5f8931815240a94444995f74d7
Carlos <carlosarturoretana3@gmail.com>
2026-02-20 19:40:42 -0600

    PrimeraSubida

---
+++ src/App.tsx
@@ -0,0 +1,88 @@
+import React, { useState } from 'react';
+import styles from './App.module.css';
+import logoApp from './assets/images/Logo.png';
+
+
+function App() {
+  const [activeTheme, setActiveTheme] = useState<'magic' | 'pokemon'>('magic');
+
+  
+  const handleToggle = () => {
+    setActiveTheme(activeTheme === 'magic' ? 'pokemon' : 'magic');
+  };
+  return (
+
+    
+    <div className={styles.splitLayout}>
+    {/* Aqui empieza el main derecha */}  
+      <div className={`${styles.sidenav} ${activeTheme === 'magic' ? 'is-magic' : 'is-pokemon'}`}>
+         <div className={styles.loginMainText}>
+            <img src={logoApp} alt="el logo deberia ir... aqui?" className={styles.logoPlaceholder} />
+            
+            <h2>Guarda Cartas<br/> (Temporal)</h2>
+            <p>Presume tus cartones brillosos</p>
+
+            <div className={styles.themeToggleContainer}>
+              <span className={`${styles.themeLabel} ${activeTheme === 'magic' ? styles.activeText : ''}`}>
+                Magic
+              </span>
+              
+              <label className={styles.switch}>
+                <input 
+                  type="checkbox" 
+                  checked={activeTheme === 'pokemon'}
+                  onChange={handleToggle}
+                />
+                <span className={styles.slider}></span>
+              </label>
+
+              <span className={`${styles.themeLabel} ${activeTheme === 'pokemon' ? styles.activeText : ''}`}>
+                Pokémon
+              </span>
+            </div>
+
+         </div>
+      </div>
+
+      {/* Aqui empieza el main derecha */}
+      <div className={styles.main}>
+         <div className={styles.loginFormContainer}>
+            
+            <div className={styles.formHeader}>
+               <h3>Bienvenidos!!!</h3>
+               <p>Ingresa a continuacion</p>
+            </div>
+
+            <form className={styles.loginForm}>
+               
+               <div className="input-group">
+                  <label>Nombre de Usuario</label>
+                  <input type="text" className="form-control" placeholder="Ejemplo: xX_Veggeta888_Xx" />
+               </div>
+               
+               <div className="input-group">
+                  <label>Contraseña</label>
+                  <input type="password" className="form-control" placeholder="••••••••" />
+                  <a href="#" className={styles.forgotPassword}>¿Olvidaste tu contraseña?</a>
+               </div>
+               
+               <div className="button-group">
+
+                  <button 
+                    type="submit" 
+                    className={`btn-primary ${activeTheme === 'magic' ? 'is-magic' : 'is-pokemon'}`}
+                  >
+                    Iniciar Sesión
+                  </button>
+                  <button type="button" className="btn-secondary">Crear Cuenta Nueva</button>
+               </div>
+
+            </form>
+         </div>
+      </div>
+      
+    </div>
+  );
+}
+
+export default App;
